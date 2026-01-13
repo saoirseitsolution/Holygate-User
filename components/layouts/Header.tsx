0df9d2e8fea50f7f4919d5a9510744396af16620
rafirafi03 <ahamedrafirafi03@gmail.com>
2026-01-13 23:53:29 +0530

    implemented container style in global css

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
 
   return (
     <header className="w-full bg-white border-b border-gray-200 fixed top-0 left-0 right-0 z-50">
-      <nav className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
+      <nav className="container mx-auto flex items-center justify-between">
         {/* Logo */}
         <div className="flex-shrink-0">
             <Image src={logo1} alt="logo" width={50} height={50} />
