25c042cc4f6957d012ab7257952146cd0d375314
rafirafi03 <ahamedrafirafi03@gmail.com>
2026-01-14 21:09:44 +0530

    fixed responsive issues of grid in nearby churches section in home pagee

--- app/(home)/_components/NearbyChurches.tsx
+++ app/(home)/_components/NearbyChurches.tsx
@@ -59,7 +59,7 @@ export default function NearbyChurches() {
         </div>
 
         {/* Churches Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-4 lg:gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-4 lg:gap-6">
           {churches.map((church, index) => (
             <div
               key={index}
